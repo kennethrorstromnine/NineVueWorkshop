b5d0ea808f7805b74d71638cfcb40f277ff07e9e
nin_kro <kro@nine.dk>
2018-10-09 18:46:47 +0200

    added ListeTodo

---
+++ src/components/ListeTodo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <b-list-group>
+      <b-list-group-item v-for="(todo, index) of $store.getters.allTodos" :key="index">        
+        <router-link :to="'/todo/'+index">{{todo}}</router-link>
+      </b-list-group-item>
+    </b-list-group>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { todoService } from "@/services/todo.service";
+
+@Component
+export default class ListeTodo extends Vue {
+}
+</script>
